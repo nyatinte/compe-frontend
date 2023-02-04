0a85ba93857f8d2a1276f3b04bb83f943fe2f2c4
nyatinte <takumi.062757@gmail.com>
2023-02-05 01:24:02 +0900

    pathpidaの導入

---
+++ src/lib/$path.ts
@@ -0,0 +1,8 @@
+export const pagesPath = {
+  mypage: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/mypage' as const, hash: url?.hash }),
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash }),
+}
+
+export type PagesPath = typeof pagesPath
